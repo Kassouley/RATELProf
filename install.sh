1d06823bba1ae5de9c0b3dda78c4a07c8a54b1b0
Kassouley <lucas.real.neto@gmail.com>
2025-02-09 10:20:12 +0000

    new install script

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ BUILD_DIR=build
 
 # Configure, build, and install
 cmake -B $BUILD_DIR -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR .
-cmake --build $BUILD_DIR
+cmake --build $BUILD_DIR 
 cmake --install $BUILD_DIR
 
 echo "Installed to $INSTALL_DIR"
