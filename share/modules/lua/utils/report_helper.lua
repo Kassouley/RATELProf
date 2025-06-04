b96ad52e62e9c6fcfea28e5715f2cfbeff3cdab5
Kassouley <lucas.real.neto@gmail.com>
2025-06-04 15:19:16 +0000

    Fix and code refrag

--- share/modules/lua/utils/report_helper.lua
+++ share/modules/lua/utils/report_helper.lua
@@ -2,18 +2,6 @@ local convert   = require ("utils.convert")
 
 local report_helper = {}
 
-local _MEM_KIND = {"Host", "Device"}
-
-function report_helper.get_copy_name_from_kind(kind)
-    return _MEM_KIND[kind+1]
-end
-
-local get_name = report_helper.get_copy_name_from_kind
-
-function report_helper.get_copy_name(src, dst)
-    return ("Copy%sTo%s"):format(get_name(src), get_name(dst))
-end
-
 function report_helper.get_duration(dur, timeunit)
     if timeunit ~= "ns" then
         return convert.time(dur, "ns", timeunit)
