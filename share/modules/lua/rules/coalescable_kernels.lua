b952e3cdd53d700001a0941fd76bb9f95786299f
Kassouley <lucas.real.neto@gmail.com>
2025-06-11 09:18:52 +0000

    [Rules] New rule : coalescable_kernels

---
+++ share/modules/lua/rules/coalescable_kernels.lua
@@ -0,0 +1,154 @@
+
+
+
+local function find_coalescable_kernels(kernel_data, gpu_node_id_map, DURATION_THRESHOLD_NS, GAP_THRESHOLD_NS, MIN_SEQUENCE_LEN, opt, app_duration)
+
+    local items = {}
+    local sequences_per_gpu = {}
+    local ideal_app_duration = app_duration
+
+    local function on_sequence_end(sequence, gpu_node_id, queue_id, name)
+        if #sequence >= MIN_SEQUENCE_LEN then
+            local sequence_start = sequence[1].start
+            local sequence_stop = sequence[#sequence].stop
+            local sequence_dur = sequence_stop - sequence_start
+            local total_gap = 0
+            for i = 2, #sequence do
+                total_gap = total_gap + (sequence[i].start - sequence[i-1].stop)
+            end
+
+
+            local ideal_duration = sequence_dur - total_gap
+            local speedup_factor = ideal_duration > 0 and (sequence_dur / ideal_duration) or 1.0
+
+            ideal_app_duration = ideal_app_duration - sequence_dur + ideal_duration
+
+            table.insert(items, {
+                gpu_node_id,
+                queue_id,
+                #sequence,
+                sequence_start,
+                sequence_stop,
+                sequence_dur,
+                total_gap,
+                tonumber(string.format("%.2f", speedup_factor)),
+                ratelprof.utils.get_kernel_name(name, opt.is_trunc, opt.is_mangled),
+            })
+
+            if not sequences_per_gpu[gpu_node_id] then
+                sequences_per_gpu[gpu_node_id] = 0
+            end
+            sequences_per_gpu[gpu_node_id] = sequences_per_gpu[gpu_node_id] + 1
+        end
+    end
+
+    -- Sort kernel data
+    local kernel_table = {}
+    for _, kernel in pairs(kernel_data) do
+        local gpu_node_id = ratelprof.utils.get_gpu_id(kernel, gpu_node_id_map)
+        local queue_id    = kernel.args.queue_id
+        
+        if not kernel_table[gpu_node_id] then kernel_table[gpu_node_id] = {} end
+        if not kernel_table[gpu_node_id][queue_id] then kernel_table[gpu_node_id][queue_id]  = {} end
+        table.insert(kernel_table[gpu_node_id][queue_id], kernel)
+
+
+    end
+
+    local last = nil
+    local sequence = {}
+
+    for gpu_node_id, kernels_per_gpu in pairs(kernel_table) do
+        for queue_id, kernels_per_queue in pairs(kernels_per_gpu) do
+            table.sort(kernels_per_queue, function(a, b) return a.start < b.start end)
+
+            for _, k in ipairs(kernels_per_queue) do
+                if k.dur > DURATION_THRESHOLD_NS then
+                    -- Skip large kernels
+                    last = nil
+                    sequence = {}
+                elseif not last or
+                    (k.args.kernel_name == last.args.kernel_name and k.start - last.stop <= GAP_THRESHOLD_NS) then
+
+                    -- Add to sequence
+                    table.insert(sequence, k)
+                else
+                    -- Check and reset
+                    on_sequence_end(sequence, gpu_node_id, queue_id, last.args.kernel_name)
+                    sequence = { k }
+                end
+                last = k
+            end
+            -- Final check for the last sequence of the queue
+            on_sequence_end(sequence, gpu_node_id, queue_id, last.args.kernel_name)
+        end
+    end
+
+    return items, sequences_per_gpu, ideal_app_duration
+end
+
+return function(traces_data, report_obj, opt)
+
+    local app_duration = traces_data.lifecycle.destructor_stop - traces_data.lifecycle.constructor_start
+
+    local DURATION_THRESHOLD_NS = tonumber(opt.report_opt.th_dur)  or ratelprof.consts._ALL_RULES_REPORT.coalescable_kernels.opt.th_dur.default
+    local GAP_THRESHOLD_NS      = tonumber(opt.report_opt.th_gap)  or ratelprof.consts._ALL_RULES_REPORT.coalescable_kernels.opt.th_gap.default
+    local MIN_SEQUENCE_LEN      = tonumber(opt.report_opt.min_seq) or ratelprof.consts._ALL_RULES_REPORT.coalescable_kernels.opt.min_seq.default
+
+    report_obj:set_name("Coalescable Kernel Launches")
+    report_obj:set_type("Analyze")
+
+    local kernel_data = traces_data:get(ratelprof.consts._ENV.DOMAIN_KERNEL)
+
+    if next(kernel_data) == nil then
+        report_obj:skip("The report could not be analyzed because it does not contain the required kernel data.")
+        return
+    end
+
+    report_obj:set_headers({
+        "GPU Node ID",
+        "Queue ID",
+        "Seq Length",
+        "Seq Start",
+        "Seq Stop",
+        "Seq Duration (ns)",
+        "Seq Gap Duration (ns)",
+        "Seq Speed Up",
+        "Kernel Name",
+    })
+
+    local data, sequences_per_gpu, ideal_app_duration = find_coalescable_kernels(kernel_data, traces_data.node_id, DURATION_THRESHOLD_NS, GAP_THRESHOLD_NS, MIN_SEQUENCE_LEN, opt, app_duration)
+
+    local speedup_factor = ideal_app_duration > 0 and (app_duration / ideal_app_duration) or 1.0
+
+    local msg = ratelprof.consts._ALL_RULES_REPORT.coalescable_kernels.desc
+
+    local advice_msg = [[
+The following kernel launch sequences may benefit from coalescing into fewer, larger launches.
+These sequences were identified as:
+  - Having the same kernel name, on the same GPU, in the same queue,
+  - Launched back-to-back with a gap smaller than ]] .. GAP_THRESHOLD_NS .. [[ ns,
+  - Each individual kernel shorter than ]] .. DURATION_THRESHOLD_NS .. [[ ns,
+  - Appearing at least ]] .. MIN_SEQUENCE_LEN .. [[ times in sequence.
+
+Optimizing these kernel calls might speed up your application by ]] .. string.format("x%.2f.\n\n", speedup_factor)
+
+    for gpu_id, count in pairs(sequences_per_gpu) do
+        advice_msg = advice_msg .. "On GPU ID " .. gpu_id .. ", " .. count .. " repeated kernel launch sequence(s) detected.\n\n"
+    end
+
+    local no_advice_msg = "\nNo redundant or coalescable kernel launch sequences were found.\n"
+
+    table.sort(data, function(a, b)
+        return a[8] > b[8]
+    end)
+
+    if #data == 0 then
+        msg = msg .. no_advice_msg
+    else
+        msg = msg .. "\n" .. advice_msg
+    end
+
+    report_obj:set_custom_message(msg)
+    report_obj:set_data(data)
+end
