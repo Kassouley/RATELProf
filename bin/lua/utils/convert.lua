1c46c8c873c466c0e4f9fe99f5690118ad14cc99
Kassouley <lucas.real.neto@gmail.com>
2025-06-24 13:49:33 +0000

    check if unit valid before convert

--- bin/lua/utils/convert.lua
+++ bin/lua/utils/convert.lua
@@ -5,15 +5,25 @@ local convert = {}
 convert.time_units = { sec = 1, ms = 1e3, us = 1e6, ns = 1e9 }
 function convert.time(value, from_unit, to_unit)
     local units = convert.time_units
-    return value * (units[to_unit] / units[from_unit])
+    local to_unit = units[to_unit]
+    local from_unit = units[from_unit]
+    if not to_unit or not from_unit then
+        error("Invalid time unit specified")
+    end
+    return value * (to_unit / from_unit)
 end
 
 
 -- Data Size Conversion Functions
 convert.bytes_units = { bytes = 1, kb = 1024, mb = 1024^2, gb = 1024^3 }
 function convert.bytes(value, from_unit, to_unit)
-    local units = convert.bytes_units 
-    return value * (units[from_unit] / units[to_unit])
+    local units = convert.bytes_units
+    local from_unit = units[from_unit]
+    local to_unit = units[to_unit]
+    if not from_unit or not to_unit then
+        error("Invalid data size unit specified")
+    end
+    return value * (from_unit / to_unit)
 end
 
 -- Number Conversion Functions
