95ea0831f6ab01488b5acde2483d76430c89b67b
Kassouley <lucas.real.neto@gmail.com>
2025-07-01 11:43:19 +0000

    support new option data-test

--- bin/lua/commands/visualize.lua
+++ bin/lua/commands/visualize.lua
@@ -14,43 +14,56 @@ function visualize.process_visualizing(positional_args, options_values)
     local tmp_dir = ratelprof.get_opt_val(options_values, "tmp")
     ratelprof.fs.mkdir(tmp_dir)
 
-    local html_file = ratelprof.fs.open_file(ratelprof.consts._HTML_REPORT_PATH, "r")
-    local html_content = html_file:read("*all")
-    html_file:close()
-
     local data = ratelprof.msgpack.decode(report_file)
-
 
     local csv_content = reports.get_csv_data(report_file, tmp_dir, data, options_values)
 
+    local b64_data = preprocess.get_b64_buffer(data, options_values)
 
-    local b64_data, string_ext_array = preprocess.get_b64_buffer(data, options_values)
     local gpu_labels, gpu_props = main_panel.get_gpu_data_as_js_string()
+
+    local string_ext_array = ratelprof.msgpack.encode.get_ext_string_table()
 
     local handlers = {
         {HANDLE = "HTML_TITLE",             sub = report_file},
-        {HANDLE = "CSV_DATA_PER_TAB",       sub = csv_content},
         {HANDLE = "LIFECYCLE_TABLE",        sub = common_data.get_lifecycle_table_as_js_string(data)},
-        {HANDLE = "EXTENSION_STRING_ARRAY", sub = string_ext_array},
+        {HANDLE = "EXTENSION_STRING_ARRAY", sub = JSON:encode(string_ext_array)},
         {HANDLE = "DOMAIN_TABLE",           sub = common_data.get_domain_table_as_js_string(data)},
         {HANDLE = "GPU_LABELS",             sub = gpu_labels},
         {HANDLE = "GPU_DATA",               sub = gpu_props},
         {HANDLE = "SUMMARY_DATA",           sub = main_panel.get_summary_data_as_js_string()},
+        {HANDLE = "CSV_DATA_PER_TAB",       sub = csv_content},
         {HANDLE = "B64_DATA",               sub = b64_data},
     }
 
-    for _, h in ipairs(handlers) do
-        html_content = html_content:gsub('"@@'..h.HANDLE..'@@"', h.sub)
-        
+    local output_file    = ratelprof.get_opt_val(options_values, "output")
+    local only_data_test = ratelprof.get_opt_val(options_values, "only-data-test")
+
+    local file = nil
+    if only_data_test then
+        output_file = output_file or ratelprof.fs.remove_extension(report_file)..'.js'
+        file = ratelprof.fs.open_file(ratelprof.consts._DATA_SET_TEST_PATH, "r")
+    else
+        output_file = output_file or ratelprof.fs.remove_extension(report_file)..'.html'
+        file = ratelprof.fs.open_file(ratelprof.consts._HTML_REPORT_PATH, "r")
     end
-    
-    local output_file = ratelprof.get_opt_val(options_values, "output")
-                            or ratelprof.fs.remove_extension(report_file)..'.html'
-    
+
+    local content = file:read("*all")
+    file:close()
+
+    for _, h in ipairs(handlers) do
+        content = content:gsub('"@@'..h.HANDLE..'@@"', h.sub)
+    end
+
     local f = ratelprof.fs.open_file(output_file, "w")
-    f:write(html_content)
+    f:write(content)
     f:close()
-    Message:print("RPROF: HTML report written to '" .. output_file.."'")
+
+    if only_data_test then
+        Message:print("RPROF: Dataset for testing written to '" .. output_file.."'")
+    else
+        Message:print("RPROF: HTML report written to '" .. output_file.."'")
+    end
 end
 
 return visualize
